fe14a3166b361f1028f060082ce760d4a385f86b
Ruchdane AMADOU <ruchdane@example.com>
2022-10-16 13:45:42 -0700

    Fix betty

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-        char c;
+	char c;
 
-        for (c = '0'; c <= '9' ; c++)
-                putchar(c);
-        for (c = 'a'; c <= 'f' ; c++)
-                putchar(c);
-        putchar('\n');
-        return (0);
+	for (c = '0'; c <= '9' ; c++)
+		putchar(c);
+	for (c = 'a'; c <= 'f' ; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
 }
