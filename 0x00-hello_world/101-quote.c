f3c47a5e08aad15d7912bc3ffe01694e0308a5e1
Ruchdane AMADOU <ruchdane@example.com>
2022-10-19 01:19:07 -0700

    j

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@ int main(void)
 	const char string1[] = "and that piece of art is useful\"";
 	const char string2[] = " - Dora Korpar, 2015-10-19\n";
 
-	fprintf(stdout,"%s%s", string1,string2);
+	fprintf(stdout, "%s%s", string1, string2);
 	return (1);
 }
