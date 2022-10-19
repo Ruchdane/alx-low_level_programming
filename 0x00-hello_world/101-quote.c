d17bea98b72136220b7550824f3f5d54660cf5f5
Ruchdane AMADOU <ruchdane@example.com>
2022-10-19 00:42:18 -0700

    optional

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include <unistd.h>
 #include <sys/syscall.h>
+
 /**
  * main - Main function
  *
@@ -8,7 +9,10 @@
  */
 int main(void)
 {
-	const char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	syscall(SYS_write, 1, string, sizeof(string));
+	const char string1[] = "and that piece of art is useful\"";
+	const char string2[] = " - Dora Korpar, 2015-10-19\n";
+
+	syscall(SYS_write, 1, string1, sizeof(string1));
+	syscall(SYS_write, 1, string2, sizeof(string2));
 	return (1);
 }
